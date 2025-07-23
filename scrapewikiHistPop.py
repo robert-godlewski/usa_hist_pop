641f60441438013152245fbd233f253e58a2ccaf
Robert Godlewski <robtechg5@gmail.com>
2025-07-22 22:36:32 -0700

    Able to save the links from wikipedia

--- scrapewikiHistPop.py
+++ scrapewikiHistPop.py
@@ -1,14 +1,11 @@
 import sqlite3
-# import pandas
 
-# from selenium.webdriver.common.by import By
-
-from useful_functions import scrapeLinks #, removeBrackets, removeParenthesis, addCensusTable
+from useful_functions import scrapeLinks
 from useful_variables import driver, usaRegionsUrl
 
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
-driver.get(usaRegionsUrl) # Need to fix this so that Wikipedia doesn't block it for calling up too many requests.
+driver.get(usaRegionsUrl)
 
 # Scraping Links
 # For the current regions in the USA with significan populations (1-57)
@@ -25,65 +22,8 @@ while t <= 14:
     scrapeLinks(xpath, driver, con, cur)
     t += 1
 
-# elementBaseXPath = "//table/tbody/tr/td/a"
-# elements = driver.find_elements(by=By.XPATH, value=elementBaseXPath)
-# regions = []
-# links = []
-# i = 0
-# while i < 57:
-#     # This is needed because there are technically only 56 regions within the USA and the USA's population and we only want the first table in this case
-#     element = elements[i]
-#     regions.append(element.get_attribute('title'))
-#     links.append(element.get_attribute('href'))
-#     i += 1
-# print('Regions:', regions)
-# print('Links:', links)
-
-# Scraping Tables - Already added this in scrapewikitables.py
-# html = driver.page_source
-# tables = pandas.read_html(html)
-
 driver.quit()
-
-# From this we want to save tables 0, 2, and 3 only
-# for table in tables: print(table)
-
 print('Found Data')
 
-# for element in elements:
-#     link = element.get_attribute('href')
-#     region = element.get_attribute('title')
-#     print('Unformatted region:', region)
-#     if '[' in region:
-#         region = removeBrackets(region)
-#     if '(' in region:
-#         region = removeParenthesis(region)
-#     if ',' in region: # This is the USA capital
-#         region = 'District of Columbia'
-#     print('Formatted region:', region)
-#     print('Link:', link)
-#     cur.execute("INSERT OR IGNORE INTO locations (name, url) VALUES ( ?, ? )", (region, link,))
-#     con.commit()
-
-# Adding in the region and link to existing table
-# i = 0
-# while i < len(regions) and i < len(links):
-#     print('Unformated region:', regions[i])
-#     if '[' in regions[i]:
-#         regions[i] = removeBrackets(regions[i])
-#     if '(' in regions[i]:
-#         regions[i] = removeParenthesis(regions[i])
-#     if ',' in regions[i]: # This is the USA capital
-#         regions[i] = 'District of Columbia'
-#     print('Formatted region:', regions[i])
-#     cur.execute("INSERT OR IGNORE INTO locations (name, url) VALUES ( ?, ? )", (regions[i], links[i],))
-#     con.commit()
-#     i += 1
-
-# Now adding in more data from the tables - Need to migrate everything here to another file to grab data from tempdb.sqlite to db.sqlite
-# t = 0
-# while t < 4: # We only need tables 0, 2, and 3. Tables 1 and 4+ are irrelevant
-#     if t == 0 or t == 2 or t == 3: addCensusTable(tables[t], con, cur)
-#     t += 1
 con.close()
 print('Saved Data')
--- useful_functions.py
+++ useful_functions.py
@@ -1,13 +1,13 @@
 # List of useful functions to help configure data
 import sqlite3
 import re
-import json
-import pandas
+# import json
+# import pandas
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 
-from .useful_variables import ADMIT_TITLE
+# from .useful_variables import ADMIT_TITLE - This is causing bugs for some reason?
 
 def removeBrackets(data: str) -> str:
     # also removes the garbage within the []
@@ -32,26 +32,26 @@ def scrapeLinks(xpath: str, driver: Chrome, con: sqlite3.Connection, cur: sqlite
         region = removeBrackets(region)
     if '(' in region:
         region = removeParenthesis(region)
-    if ',' in region: # This is the USA capital
+    if ',' in region: # This is the USA capital - Need more of a specific because there's one entry that didn't work.
         region = 'District of Columbia'
     print('Formatted region:', region)
     print('Link:', link)
     cur.execute("INSERT OR IGNORE INTO locations (name, url) VALUES ( ?, ? )", (region, link,))
     con.commit()
 
-def addRegionAdmittance(row, location, table: pandas.DataFrame, con: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
-    if ADMIT_TITLE in table.columns:
-        has_admit = True
-    else:
-        has_admit = False
-    if has_admit and 'id' in location:
-        admit_raw = table.at[row, ADMIT_TITLE]
-        if admit_raw:
-            admitted = int(admit_raw)
-        else:
-            admitted = 1776 # Historical formation of the USA
-        cur.execute("UPDATE locations SET admitted = ? WHERE id = ? ", (admitted, location['id'],))
-        con.commit()
+# def addRegionAdmittance(row, location, table: pandas.DataFrame, con: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
+#     if ADMIT_TITLE in table.columns:
+#         has_admit = True
+#     else:
+#         has_admit = False
+#     if has_admit and 'id' in location:
+#         admit_raw = table.at[row, ADMIT_TITLE]
+#         if admit_raw:
+#             admitted = int(admit_raw)
+#         else:
+#             admitted = 1776 # Historical formation of the USA
+#         cur.execute("UPDATE locations SET admitted = ? WHERE id = ? ", (admitted, location['id'],))
+#         con.commit()
 
 def saveCensusData(location, value, col_name: str, con: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
     if '[' in col_name:
@@ -71,18 +71,18 @@ def saveCensusData(location, value, col_name: str, con: sqlite3.Connection, cur:
     cur.execute("INSERT OR IGNORE INTO census (year, population, location_id) VALUES ( ?, ?, ? )", (year, pop, location['id'],))
     con.commit()
 
-def addCensusTable(table: pandas.DataFrame, con: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
-    for index, row in table.iterrows():
-        print(f'Index: {index}')
-        print('Row:', row)
-        # print('Name:', table[row]['Name'])
-        name = table.at[row, 'Name'] # This is breaking for some reason - Review https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.at.html#pandas.DataFrame.at
-        if '[' in name:
-            name = removeBrackets(name)
-        cur.execute("SELECT * FROM locations WHERE name = ? ", (name,))
-        location_raw = cur.fetchone()[0]
-        location = json.loads(location_raw)
-        addRegionAdmittance(row, location, table, con, cur)
-        for col_name, value in row.items():
-            if col_name is not 'Name' and col_name != ADMIT_TITLE:
-                saveCensusData(location, value, col_name, con, cur)
+# def addCensusTable(table: pandas.DataFrame, con: sqlite3.Connection, cur: sqlite3.Cursor) -> None:
+#     for index, row in table.iterrows():
+#         print(f'Index: {index}')
+#         print('Row:', row)
+#         # print('Name:', table[row]['Name'])
+#         name = table.at[row, 'Name'] # This is breaking for some reason - Review https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.at.html#pandas.DataFrame.at
+#         if '[' in name:
+#             name = removeBrackets(name)
+#         cur.execute("SELECT * FROM locations WHERE name = ? ", (name,))
+#         location_raw = cur.fetchone()[0]
+#         location = json.loads(location_raw)
+#         addRegionAdmittance(row, location, table, con, cur)
+#         for col_name, value in row.items():
+#             if col_name is not 'Name' and col_name != ADMIT_TITLE:
+#                 saveCensusData(location, value, col_name, con, cur)
